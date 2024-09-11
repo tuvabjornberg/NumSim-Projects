638d74a28d453802991b4a9dc4271c1c9f863762
Tuva <tuva.bjornberg.5452@student.uu.se>
2024-09-11 08:40:05 +0200

    v(t) suggestion

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import numpy as np 
+import numpy as np
 import scipy
+from scipy.integrate import solve_ivp
+
+g = 9.82
+c = 0.05
+k = 700
+
 
 def mass(t):
     if t <= 10:
@@ -7,16 +13,43 @@ def mass(t):
     else:
         return 4
 
-def engine_dir(t):
-    return t
 
-def velocity(t):
-    k = 700
-    direction = engine_dir(t)
+def mass_der(t):
+    if t <= 10:
+        return -0.4
+    else:
+        return 0
+
+
+def engine_dir(t, height):
+    if height < 20:
+        return np.pi / 2
+    else:
+        return t  # TODO: Stub
+
+
+def fuel_velocity(t, height):
+    direction = engine_dir(t, height)
     x = k * np.cos(direction)
     y = k * np.sin(direction)
     return (x, y)
 
-def external_forces(t):
-    mass = mass(t)
-    
+
+def external_forces(t, v):
+    m = mass(t)
+    gravity = (0, m * (-g))  # Gravity vector: x, y direction
+    air_res = c * np.linalg.norm(v) * v  # Air resistance: c||v(t)||v(t)
+    return gravity + air_res
+
+
+def ode_rhs(t, v, height):
+    # dv(t)/dt = g - c||v(t)||v(t) + m'(t)u(t)/m(t)
+    # --> dv(t)/dt = (external_forces() + mass_der()fuel_velocity())/mass()
+
+    m = mass(t)
+    md = mass_der(t)
+    fv = np.array(fuel_velocity(t, height))
+    ext_f = np.array(external_forces(t, v))
+
+    dvdt = (ext_f + md * fv) / m
+    # return dvdt
--- test.py
+++ test.py
@@ -1,18 +1,56 @@
 import main
+import numpy as np
+
 
 def test_mass():
     assert main.mass(30) == 4
     assert main.mass(5) == 6.0
     assert main.mass(0) == 8
-    
-def test_velocity():
-    assert main.velocity(30) == (107.97601492130883, -691.6221368650033)
-    assert main.velocity(0) == (700, 0)
-    
-#def test_external_forces():
-#    assert main.external_forces(30) == 4
+
+
+def test_mass_der():
+    assert main.mass_der(5) == -0.4
+    assert main.mass_der(15) == 0
+
+
+def test_engine_dir():
+    assert main.engine_dir(5, 10) == np.pi / 2
+    assert main.engine_dir(25, 30) == 25  # TODO: Stub in engine dir height >= 20
+
+
+def test_fuel_velocity():
+    np.testing.assert_allclose(
+        main.fuel_velocity(30, 10), (4.2862637970157365e-14, 700.0)
+    )
+    np.testing.assert_allclose(
+        main.fuel_velocity(0, 0), (4.2862637970157365e-14, 700.0)
+    )
+    np.testing.assert_allclose(
+        main.fuel_velocity(40, 20), (-466.85664315658335, 521.5792123355442)
+    )  # TODO: Stub in engine dir
+
+
+def test_external_forces():
+    v = np.array([0, 0])
+    t = 5
+    expected_forces = (0, -58.92)
+    ext_forces = main.external_forces(t, v)
+    np.testing.assert_allclose(ext_forces, expected_forces, atol=1e-2)
+
+
+def test_ode_rhs():
+    t = 5
+    v = np.array([0, 0])
+    height = 10
+    rhs_value = main.ode_rhs(t, v, height)
+    assert rhs_value is not None
+
 
 test_mass()
-test_velocity()
-#test_external_forces()
+test_mass_der()
+test_engine_dir()
+test_fuel_velocity()
+test_external_forces()
+test_ode_rhs()
+
 print("All tests passed")
