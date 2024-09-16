78ea077ad97179fd43a085360e79d5abcda662e7
Tuva <tuva.bjornberg02@gmail.com>
2024-09-16 16:00:53 +0200

    version  1 OK! Co-authored-by: nore1770 <nore1770@users.noreply.github.com> Co-authored-by: mast1554 <mast1554@users.noreply.github.com>

--- main.py
+++ main.py
@@ -7,51 +7,47 @@ g = 9.82
 c = 0.05
 k = 700
 
+burn_rate = 0.4
+initial_rocket_mass = 8
+initial_fuel_mass = 4
+t_max = initial_fuel_mass / burn_rate
+
 target_x = 80
 target_y = 60
-initial_rocket_mass = 8
+target = (target_x, target_y)
 
-t_span = (0, 100)  # time intervall
-initial_state = [
-    0,
-    0,
-    0,
-    0,
-    initial_rocket_mass,
-]  # [x_pos, y_pos, x_velocity, y_velocity, mass]
+t_span = (0, 10)  # time intervall
+initial_state = [0, 0, 0, 0]  # [x_pos, y_pos, x_velocity, y_velocity, mass]
 
 
 def mass(t):
-    if t <= 10:
-        return 8 - 0.4 * t
+    if t <= t_max:  # 4 / 0.4
+        return initial_rocket_mass - burn_rate * t
     else:
-        return 4
+        return initial_rocket_mass - initial_fuel_mass
 
 
 def mass_der(t):
-    if t <= 10:
-        return -0.4
+    if t <= t_max:
+        return -burn_rate
     else:
         return 0
 
 
-def engine_dir(t, x, y):
+def engine_dir(t, x, y, target):
+    x_t, y_t = target
     if y < 20:
         return -np.pi / 2
     else:
-        Ground = np.array(60, 0)
-        v = np.array([x, y])
-        v_len = np.sqrt((60 - x) ** 2 + (80 - y) ** 2)
-
-        angle = np.arccos((v.dot(Ground)) / v_len * 60)
-        return angle + 2*np.pi
+        angle = np.tan((y_t - y) / (x_t - x))
+        return angle + np.pi
 
 
-def fuel_velocity(t, x, y):
-    if t > 10:
+def fuel_velocity(t, x, y, target, steering_func):
+    if t > t_max:
         return np.array([0, 0])
     else:
-        direction = engine_dir(t, x, y)
+        direction = steering_func(t, x, y, target)
         vx = k * np.cos(direction)
         vy = k * np.sin(direction)
         return np.array([vx, vy])
@@ -59,57 +55,77 @@ def fuel_velocity(t, x, y):
 
 def external_forces(t, v):
     m = mass(t)
-    gravity_F = (0, m * (-g))  # Gravity vector: x, y direction
+    gravity_F = m * np.array([0, -g])  # Gravity vector: x, y direction
     air_res = -c * np.linalg.norm(v) * v  # Air resistance: c||v(t)||v(t)
-    return np.array([gravity_F + air_res])
+    return gravity_F + air_res
 
 
-def rocket_velocity(t, v, state):
-    # dv(t)/dt = g - c||v(t)||v(t) + m'(t)u(t)/m(t)
-    # --> dv(t)/dt = (external_forces() + mass_der()fuel_velocity())/mass()
-    x, y, vx, vy, m = state
-    v = np.array([vx, vy])
+def rocket_ODE(t, y, steering_func):
+    x_pos, y_pos, vx, vy = y
 
     m = mass(t)
-    md = mass_der(t)
-    fv = fuel_velocity(t, x, y)
-    ext_f = external_forces(t, v)
-
-    dvdt = (ext_f + md * fv) / m
-    return dvdt
-
-
-def func(t, state):
-    x, y, vx, vy, m = state
-
     v = np.array([vx, vy])
+
     ext_forces = external_forces(t, v)
-    velocity = fuel_velocity(t, x, y)
+    engine_forces = mass_der(t) * fuel_velocity(t, x_pos, y_pos, target, steering_func)
+    total_force = ext_forces + engine_forces
 
-    total_force_x = ext_forces[0] + velocity[0]
-    total_force_y = ext_forces[1] + velocity[1]
+    acc = total_force / m
 
-    acc_x = total_force_x * m
-    acc_y = total_force_y * m
-
-    mass_change_rate = mass_der(t)
-
-    state_derivatives = [
-        vx,
-        vy,
-        acc_x,
-        acc_y,
-        mass_change_rate
-    ]
-
+    state_derivatives = [vx, vy, acc[0], acc[1]]
     return state_derivatives  # [x_pos, y_pos, x_velocity, y_velocity, m]
 
 
-sol = solve_ivp(func, t_span, initial_state)
+def RK4(f, tspan, u0, dt, *args):
+    t_vec = np.arange(tspan[0], tspan[1] + 1.0e-14, dt)
+    dt_vec = dt * np.ones_like(t_vec)
+    if t_vec[-1] < tspan[1]:
+        t_vec = np.append(t_vec, tspan[1])
+        dt_vec = np.append(dt_vec, t_vec[-1] - t_vec[-2])
 
-plt.plot(sol.t, sol.y[0])
+    u = np.zeros((len(t_vec), len(u0)))
+    u[0, :] = u0
+    for i in range(len(t_vec) - 1):
+        h = dt_vec[i]
+        k1 = np.array(f(t_vec[i], u[i, :], *args))
+        k2 = np.array(f(t_vec[i] + 0.5 * h, u[i, :] + 0.5 * h * k1, *args))
+        k3 = np.array(f(t_vec[i] + 0.5 * h, u[i, :] + 0.5 * h * k2, *args))
+        k4 = np.array(f(t_vec[i + 1], u[i, :] + h * k3, *args))
+        u[i + 1, :] = u[i, :] + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+    return t_vec, u
+
+
+tt = np.arange(t_span[0], t_span[1], 0.1)
+sol = solve_ivp(rocket_ODE, t_span, initial_state, args=(engine_dir,), t_eval=tt)
+t, u = RK4(rocket_ODE, t_span, initial_state, 0.1, engine_dir)
+
+# --------- X-Y TO TIME AXIS ---------
+plt.plot(sol.t, sol.y[0], label="x position")
+plt.plot(sol.t, sol.y[1], label="y position")
 plt.title("Rocket steering")
 plt.xlabel("Position x ()")
 plt.ylabel("Position y ()")
+plt.grid()
+plt.legend()
+plt.show()
+
+# --------- X-Y POS AXES ---------
+plt.plot(sol.y[0], sol.y[1])
+plt.title("Rocket steering - xy pos")
+plt.xlabel("Position x ()")
+plt.ylabel("Position y ()")
+plt.xlim([-5, 100])
+plt.ylim([-5, 100])
+plt.grid()
+plt.prism()
+plt.show()
+
+# --------- RUNGEKUTTA ---------
+plt.plot(u[:, 0], u[:, 1])
+plt.title("Rocket steering - xy pos RUNGEKUTTA")
+plt.xlabel("Position x ()")
+plt.ylabel("Position y ()")
+plt.xlim([-5, 100])
+plt.ylim([-5, 100])
 plt.grid()
 plt.show()
